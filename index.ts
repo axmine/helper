9baaa5dcbafa30ea88856d861740b47ca0bef49d
Axmine <yeahoor@qq.com>
2021-04-07 16:41:13 +0800

    新入口文件

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { cookie } from './lib/cookie';
+import { getType } from './lib/getType';
+import { random } from './lib/random';
+import { Storage } from './lib/storage';
+import date from './lib/date'
+
+// export default { cookie, random, getType, Storage }
+export {
+  cookie,
+  random,
+  getType,
+  date,
+  Storage
+}
