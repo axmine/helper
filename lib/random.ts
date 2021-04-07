7949d2570334e76bcdcd3b90c7eac0722d5fd471
Axmine <yeahoor@qq.com>
2021-04-07 16:44:36 +0800

    获取指定范围的随机数

--- lib/random.ts
+++ lib/random.ts
@@ -6,12 +6,12 @@ import { getType } from './getType'
  * @param type 确定返回类型为浮点数或整型
  * @returns number
  */
-export function random (min: number = 0, max: number = 1, type: 'float'|'int'): number {
+export function random (min: number = 0, max: number = 1, result: 'float'|'int' = 'float'): number {
   if (getType(min) !== 'number' || getType(max) !== 'number') {
-    throw new Error('仅支持数字随机')
+    throw new Error('仅支持数字随机');
   }
-  if (min >= max) { throw new Error('请输入合理的随机范围'); }
+  if (min > max) { throw new Error('请输入合理的随机范围'); }
   let res = Math.random() * (max - min + 1) + min;
-  if (type === 'int') { res = Math.floor(res); }
+  if (result === 'int') { res = Math.floor(res); }
   return res;
 }
